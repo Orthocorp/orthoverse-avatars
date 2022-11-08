6239c5b4f87404f04b4cc41d417cf2bfc820e168
Richard Piacentini <richard.piacentini@nuxos.asia>
2022-11-09 01:50:52 +0700

    Add chakra container

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,18 @@
+import { Container } from '@chakra-ui/react'
 import { ConnectKitButton } from 'connectkit'
 
-import { Link, routes } from '@redwoodjs/router'
+import { Link } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Orthoverse Avatars" description="Orthoverse Avatars" />
+      <Container>
+        <h1>Orthoverse Avatars</h1>
 
-      <h1>Orthoverse Avatars</h1>
-
-      <ConnectKitButton />
+        <ConnectKitButton />
+      </Container>
     </>
   )
 }
