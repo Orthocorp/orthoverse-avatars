b8f122b2ae3c9b548c8761418c549675d016606f
Richard Piacentini <richard.piacentini@nuxos.asia>
2022-11-09 01:36:44 +0700

    Add support for Goerli

--- web/src/layouts/DefaultLayout/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -4,15 +4,20 @@ import {
   getDefaultClient,
 } from 'connectkit'
 import { WagmiConfig, createClient } from 'wagmi'
+import { chain } from 'wagmi'
 
 type DefaultLayoutProps = {
   children?: React.ReactNode
 }
+// Choose which chains you'd like to show
+const chains = [chain.mainnet, chain.goerli]
+
 const alchemyId = process.env.ALCHEMY_ID
 const client = createClient(
   getDefaultClient({
     appName: 'Orthoverse Avatars',
     alchemyId,
+    chains,
   })
 )
 
