33b98b2dd47f7f52cda65f38184698e9743d27d3
Keir Finlow-Bates <keir@chainfrog.com>
2023-03-31 09:24:40 +0300

    Dynamic landpane loading with bugs

--- web/src/components/LandPane/LandPane.tsx
+++ web/src/components/LandPane/LandPane.tsx
@@ -22,118 +22,53 @@ import {
   BsArrowCounterclockwise
 } from 'react-icons/bs'
 
+import axios from 'axios'
+
 import { useAuth } from '@redwoodjs/auth'
+import { Spinner } from '@chakra-ui/react'
 
 const LandPane = () => {
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ test values
-  const testA = {
-    cape: '0x2830b5a3b5242bc2c64c390594ed971e7ded47d2',
-    highest_level: 7,
-    land_name: 'Pencanlia',
-    lands: [
-      [
-        'Telastala',
-        '0x2ccc96b3690f88f05b1b99319c4ecfce033dddd5',
-        9,
-        -29,
-        31,
-        'The Prefecture of Telastala',
-      ],
-      [
-        'Dundallia',
-        '0x2830b5a3b5242bc2c64c390594ed971e7ded47d2',
-        4,
-        -30,
-        31,
-        'The Croft of Dundallia',
-      ],
-      [
-        'Fangwanina',
-        '0x60515c2da6c76ef0b092534fa5be84b07e4da689',
-        7,
-        0,
-        -1,
-        'The Kingdom of Fangwania',
-      ],
-      [
-        'Hathforsmia',
-        '0x766c94a76f3652b85358da0d5c039635e20c27d7',
-        5,
-        -31,
-        -1,
-        'The Duchy of Hathforsmia',
-      ],
-      [
-        'Unarbsia',
-        '0x72d0474ea276e628072624116caea7f05e2b33f9',
-        0,
-        2,
-        -34,
-        'The Croft of Unarbsia',
-      ],
-      [
-        'Pencanlia',
-        '0x4747477222244233277233244222277474740000',
-        7,
-        0,
-        31,
-        'The Kingdom of Pencanlia',
-      ],
-      [
-        'Pendasdor',
-        '0xcd2081c2e433120cdaa70f4af01fe0b8c53a791c',
-        0,
-        5,
-        -4,
-        'The Croft of Pensador',
-      ],
-      [
-        'Clandiburium',
-        '0x2c36dd7bb3e95e7a0219e70737ee8041f22d2081',
-        0,
-        -35,
-        -33,
-        'The Croft of Clandiburium',
-      ],
-      [
-        'Urvoefia',
-        '0x98fa77ec842acd58298e719dd50fefcab9caad1a',
-        0,
-        23,
-        -6,
-        'The Croft of Urvoefia',
-      ],
-    ],
-  }
-
-  const testB = {}
-
-  const testC = {
-    highest_level: 7,
-    land_name: 'Pencanlia',
-    cape: '0x2830b5a3b5242bc2c64c390594ed971e7ded47d2',
-    lands: [
-      [
-        'Telastala',
-        '0x2ccc96b3690f88f05b1b99319c4ecfce033dddd5',
-        9,
-        -29,
-        31,
-        'The Prefecture of Telastala',
-      ],
-    ],
-  }
-
-  const test1 = testA
-
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ test values
-
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   const [paneItem, setPaneItem] = useState(-1)
   const [usedCape, setUsedCape] = useState('')
 
   const { currentUser, isAuthenticated } = useAuth()
+
+  const [loading, setLoading] = useState('loading');
+  const [data, setData] = useState([])
+
+  const [test1, setTest1] = useState({})
+
+  function parseLandReturn (data) {
+    if (data === {}) return data
+    let temp = {lands: []}
+    let highest = 0
+    let highestName = ''
+    let lands = []
+    let cape = '0x00000000000000000000000000000000'
+    data.forEach((land)  => {
+      const processedLand = [
+        land.name,
+        land.tokenId,
+        land.level,
+        land.x,
+        land.y,
+        land.description
+      ]
+      if (land.level % 8 > highest) {
+        highest = land.level
+        highestName = land.name
+      }
+      lands.push(processedLand)
+    })
+    temp.lands = lands
+    temp.highest_level = highest
+    temp.land_name = highestName
+    console.log(temp)
+    setPaneItem(0)
+    return temp
+  }
 
   useEffect(() => {
     if (currentUser !== 'undefined') {
@@ -142,10 +77,27 @@ const LandPane = () => {
       // for testing the display I will initially use test1 or test2
       //if (currentUser.lands === {}) {
 
-      if (Object.keys(test1).length !== 0) {
-        setPaneItem(0)
-        setUsedCape(test1.cape)
+      const fetchData = async () => {
+        setLoading('loading')
+        try {
+          console.log('https://orthoverse.io/api/land/owned?owner=' + currentUser.address)
+          const { data: response} =
+            await axios.get('https://orthoverse.io/api/land/owned?owner=' + currentUser.address)
+          console.log(response)
+          setTest1(parseLandReturn(response))
+        } catch (error) {
+          console.log(error.message)
+          setLoading('error')
+        }
+        setLoading('success')
       }
+
+      fetchData().then(result => {
+        if (Object.keys(test1).length !== 0) {
+          setPaneItem(0)
+          setUsedCape(test1.cape)
+        }
+      })
     }
   }, [])
 
@@ -153,7 +105,8 @@ const LandPane = () => {
     if (paneItem !== -1) {
       // this is where the cape data should probably be written
     }
-  }, [paneItem, usedCape])
+    console.log("Loading status: " + loading)
+  }, [paneItem, usedCape, test1, loading])
 
   function goLeft() {
     console.log('Clicked left')
@@ -206,7 +159,49 @@ const LandPane = () => {
   }
 
   if (isAuthenticated && typeof currentUser !== 'undefined') {
-    if (paneItem === -1) {
+    if (loading === 'loading') {
+      return (
+        <>
+          <Box>
+            <Button colorScheme="teal">Show Land</Button>
+          </Box>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Loading</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Box p="4px">
+                  Loading your lands. Bear with us...
+                </Box>
+                <Box p="4px">
+                  <Spinner />
+                </Box>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+    } else if (loading === 'error') {
+      return (
+        <>
+          <Box>
+            <Button colorScheme="teal">Show Land</Button>
+          </Box>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Error</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                Something went wrong while loading your lands. Try refreshing, and
+                if that fails, please contact BCGandalf or TheOtherGuy.
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+    } else if (test1 === {}) {
       return (
         <>
           <Box>
@@ -435,7 +430,7 @@ const LandPane = () => {
       )
     }
   } else {
-    ;<>
+    <>
       <Box>
         <Button colorScheme="teal">Show Land</Button>
       </Box>
